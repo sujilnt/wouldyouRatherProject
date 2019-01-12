6c313b4328175c6da8b2616aef9fe04b878f3810
sujilnt <sujilnt@gmail.com>
2019-01-12 23:51:35 +0000

    adding a microsoft fabric library

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 const LoginPage = ()=>(
 		<div>
 		<h1>Login Component </h1>
+			<DefaultButton
+				text='See Button'
+				primary={ true }
+				href='#/components/button'
+			/>
 	</div>
 );
 export default LoginPage;
