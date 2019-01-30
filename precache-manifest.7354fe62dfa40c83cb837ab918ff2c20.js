6b752d1d06a9bfd91b0f4bf3e0ac7b27eb13aa05
sujilnt <sujilnt@gmail.com>
2019-01-30 11:56:32 +0000

    Updates

---
+++ precache-manifest.7354fe62dfa40c83cb837ab918ff2c20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63351eb5c6357cad9cdf",
+    "url": "/WouldYouRatherProject/static/js/runtime~main.63351eb5.js"
+  },
+  {
+    "revision": "0e4a1571f71128e60422",
+    "url": "/WouldYouRatherProject/static/js/main.0e4a1571.chunk.js"
+  },
+  {
+    "revision": "59cd2fab4ea6df5a3b0b",
+    "url": "/WouldYouRatherProject/static/js/1.59cd2fab.chunk.js"
+  },
+  {
+    "revision": "0e4a1571f71128e60422",
+    "url": "/WouldYouRatherProject/static/css/main.f21bdeb2.chunk.css"
+  },
+  {
+    "revision": "bb40822eefec1789c1071b053371fd59",
+    "url": "/WouldYouRatherProject/index.html"
+  }
+];
