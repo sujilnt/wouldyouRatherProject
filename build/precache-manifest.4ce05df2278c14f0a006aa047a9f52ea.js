e7a1b426eec2ed11eac8c8e1255e6d4b102f1ffe
sujilnt <sujilnt@gmail.com>
2019-02-02 15:16:15 +0000

    just adding builds

---
+++ build/precache-manifest.4ce05df2278c14f0a006aa047a9f52ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d56731fec82ec04ad63",
+    "url": "/wouldyouRatherProject/static/js/runtime~main.0d56731f.js"
+  },
+  {
+    "revision": "3acf39c84966e07177f1",
+    "url": "/wouldyouRatherProject/static/js/main.3acf39c8.chunk.js"
+  },
+  {
+    "revision": "dbd7f1a281f238d5ce40",
+    "url": "/wouldyouRatherProject/static/js/1.dbd7f1a2.chunk.js"
+  },
+  {
+    "revision": "3acf39c84966e07177f1",
+    "url": "/wouldyouRatherProject/static/css/main.10a41bbb.chunk.css"
+  },
+  {
+    "revision": "40844765bb817a272fa4af80a739aff1",
+    "url": "/wouldyouRatherProject/index.html"
+  }
+];
